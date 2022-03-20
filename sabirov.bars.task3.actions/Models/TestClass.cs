2a3ec48e8fbc4fe12adec5705350d099ed7ad7b4
Zulfat <zulfat.sabirov.04@bk.ru>
2022-03-20 21:49:36 +0300

    Добавьте файлы проекта.

---
+++ sabirov.bars.task3.actions/Models/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sabirov.bars.task3.actions.Models
+{
+    public class TestClass
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public event Action<Exception> OnError;
+        private static char exitSymbol = 'c';
+        public void Run()
+        {
+            Console.WriteLine($"Здравствуйте! Чтобы выйти из цикла, необходимо ввести в консоль символ \"{exitSymbol}\".");
+            while (true)
+            {
+                try
+                {
+                    var msg = Convert.ToChar(Console.ReadLine());
+                    if (msg == exitSymbol)
+                    {
+                        OnKeyPressed.Invoke(this, msg);
+                        Console.WriteLine("ну вот и все.. конец");
+                        break;
+                    }
+                    Console.WriteLine($"это не символ \"{exitSymbol}\", жду следующего сообщения...");
+
+                }
+                catch (Exception e)
+                {
+                    OnError.Invoke(e);
+                }
+
+            }
+        }
+    }
+}
---
+++ sabirov.bars.task3.actions/Program.cs
@@ -0,0 +1,26 @@
+﻿using sabirov.bars.task3.actions.Models;
+using System;
+
+namespace sabirov.bars.task3.actions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            testClass.OnKeyPressed += TestClass_OnKeyPressed;
+            testClass.OnError += TestClass_OnError;
+            testClass.Run();
+        }
+
+        private static void TestClass_OnError(Exception obj)
+        {
+            Console.WriteLine($"Упс... ошибка {obj.Message}");
+        }
+
+        private static void TestClass_OnKeyPressed(object sender, char e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
